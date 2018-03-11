ceb467af842d9e8b90011d08efd853bee0ab4ca3
fxyf1991 <fxyf1991@gmail.com>
2018-03-11 10:31:28 +0800

    编辑歌曲保持高亮

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -7,18 +7,19 @@
         `,
         render(data) {
             $(this.el).html(this.template)
-            let {songs} = data
-            let liList = songs.map((song)=> $('<li></li>').text(song.name).attr('data-song-id',song.id))
+            let {songs, selectSongId} = data
+            let liList = songs.map((song)=> {
+                let $li = $('<li></li>').text(song.name).attr('data-song-id',song.id)
+                if(song.id === selectSongId){
+                    $li.addClass('active')
+                }
+                return $li
+            })
             let $el = $(this.el)
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
-        },
-        activeItem(li){
-            let $li = $(li)
-            $li.addClass('active')
-                .siblings('.active').removeClass('active')
         },
         clearActive() {
             $(this.el).find('.active').removeClass('active')
@@ -26,7 +27,8 @@
     }
     let model = {
         data: {
-            songs: []
+            songs: [],
+            selectSongId:undefined
         },
         find(){
             var query = new AV.Query('Song')
@@ -54,8 +56,9 @@
         },
         bindEvents(){
             $(this.view.el).on('click', 'li', (e)=>{
-                this.view.activeItem(e.currentTarget)
                 let songId = e.currentTarget.getAttribute('data-song-id')
+                this.model.data.selectSongId = songId
+                this.view.render(this.model.data)
                 let data
                 let songs = this.model.data.songs
                 for (let i = 0; i < songs.length; i++){
